e1f7ae011f193e454b04e3166b1f6db5e9455cb7
Sibo Wang <sibo.wang@epfl.ch>
2023-09-15 13:20:24 +0200

    normalize speed by flat terrain, increas sim time to 1.5s

--- hybrid_controller/generate_all_data.sh
+++ hybrid_controller/generate_all_data.sh
@@ -3,6 +3,8 @@ export PYOPENGL_PLATFORM=egl
 
 for arena in flat gapped blocks mixed; do
     echo "Running test on $arena arena"
-    python generate_datapts.py --arena=$arena --adhesion --n_exp=10 --n_procs=1
+    python generate_datapts.py --arena=$arena --adhesion --n_exp=10 --n_procs=1 &
     # python generate_datapts.py --arena=$arena --n_exp=10 --n_procs=1
 done
+
+wait $(jobs -p)
