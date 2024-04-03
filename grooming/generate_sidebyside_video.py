fb4bcb6b889f6faf63f8654e30d9a6eea24ef3f9
stimpfli <victor.stimpfling@epfl.ch>
2024-04-03 15:17:55 +0200

    v0 of making grooming video

---
+++ grooming/generate_sidebyside_video.py
@@ -0,0 +1,169 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from pathlib import Path
+from tqdm import trange
+
+from flygym import SingleFlySimulation
+from flygym.arena.tethered import Tethered
+from nmf_grooming import all_groom_dofs, GroomingCamera, GroomingFly
+
+import cv2
+
+timestep = 1e-4
+
+
+def get_data(xml_variant):
+    data_path = Path(f"/Users/stimpfli/Desktop/nmf2-paper/grooming/data/220807_aJO-Gal4xUAS-CsChr_fly002_beh002/pose-3d/{xml_variant}")
+    with open(data_path / "leg_joint_angles.pkl", "rb") as f:
+        leg_data = pickle.load(f)
+
+    with open(data_path / "head_joint_angles.pkl", "rb") as f:
+        head_data = pickle.load(f)
+
+    # merge the dict (have different keys)
+    data_raw = {**leg_data, **head_data}
+
+    data_raw["meta"] = {"timestep":1e-2}
+    # reformat the data
+    joint_corresp = {"ThC":"Coxa", "CTr":"Femur", "FTi":"Tibia", "TiTa":"Tarsus1"}
+
+    data = {}
+    for key, value in data_raw.items():
+        if key == "meta":
+            data[key] = value
+            continue
+        elif "head" in key:
+            _, joint, dof = key.split("_")
+            side = ""
+            joint = "Head"
+        elif "antenna" in key:
+            _, joint, dof, side = key.split("_")
+            joint = "Pedicel"
+        else:
+            _, side, joint, dof = key.split("_")
+            joint = joint_corresp[joint]
+        new_key = f"joint_{side}{joint}"
+        if not dof == "pitch":
+            new_key += f"_{dof}"
+        
+        data[new_key] = value
+
+    target_num_steps = int(len(data["joint_LFCoxa"])/timestep*data["meta"]["timestep"])
+    data_block = np.zeros((len(all_groom_dofs), target_num_steps))
+    input_t = np.arange(len(data["joint_LFCoxa"])) * data["meta"]["timestep"]
+    output_t = np.arange(target_num_steps) * timestep
+    for i, joint in enumerate(all_groom_dofs):
+        if "RPedicel_yaw" in joint:
+                # data[joint] = np.ones_like(data[joint])*np.mean(data[joint])*-1
+                data[joint] *= -1
+        data_block[i, :] = np.unwrap(np.interp(output_t, input_t, data[joint]))
+
+    # swap head roll and head yaw
+    hroll_id = all_groom_dofs.index("joint_Head_roll")
+    hyaw_id = all_groom_dofs.index("joint_Head_yaw")
+    hroll = data_block[hroll_id, :].copy()
+    data_block[hroll_id, :] = data_block[hyaw_id, :]
+
+    return data_block
+
+def get_replay_frames(data_block_focus, video_fps):
+
+    fly = GroomingFly(xml_variant=xml_variant)
+    cam = GroomingCamera(fly, camera_id="Animat/camera_front")
+    cam._eff_render_interval = 1 / video_fps
+    sim = SingleFlySimulation(
+        fly=fly,
+        cameras=[cam],
+        arena=Tethered(),
+    )
+
+    timestamps = []
+    _ = sim.reset()
+
+    for i in range(50):
+        sim.step({"joints": data_block_focus[:, 0]})
+
+    sim.curr_time = 0.0
+    for i in trange(target_num_steps):
+        # here, we simply use the recorded joint angles as the target joint angles
+        joint_pos = data_block_focus[:, i]
+        action = {"joints": joint_pos}
+        _ = sim.step(action)
+        frame = sim.render()
+        if frame:
+            timestamps.append(sim.curr_time)
+
+    return cam._frames, timestamps
+
+def get_original_frames(start_t, end_t, video_fps):
+    video = Path("/Users/stimpfli/Desktop/nmf2-paper/grooming/data/220807_aJO-Gal4xUAS-CsChr_fly002_beh002/videos/camera_3.mp4")
+    # Extract frames between start and end time
+
+    cap = cv2.VideoCapture(str(video))
+    cap.set(cv2.CAP_PROP_POS_MSEC, start_t * 1000)
+    num_frames = int((end_t - start_t) * video_fps)
+
+    base_video_frames = []
+    timestamp = []
+
+    for i in range(num_frames):
+        ret, frame = cap.read()
+        if not ret:
+            break
+        base_video_frames.append(frame)
+        timestamp.append(cap.get(cv2.CAP_PROP_POS_MSEC)*1000)
+
+    cap.release()
+    return base_video_frames, timestamp
+
+def write_side_by_side_video(order, output_path, replay_frames, replay_timestamps, video_fps):
+    assert len(replay_frames[order[0]]) == len(replay_frames[order[1]]), f"Length mismatch between {order[0]} and {order[1]} ({replay_timestamps[order[0]]} vs {replay_timestamps[order[1]]}"
+    assert len(replay_frames[order[0]]) == len(replay_frames[order[2]]), f"Length mismatch between {order[0]} and {order[2]} ({replay_timestamps[order[0]]} vs {replay_timestamps[order[2]]}"
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    # write a video with the three frames side by side
+    out_size = [0, 0]
+    for key in order:
+        frame_shape = replay_frames[key][0].shape
+        out_size[0] += frame_shape[1]
+    out_size[1] = replay_frames["real_fly"][0].shape[0] # assume all frames have the same height
+    
+    out = cv2.VideoWriter(str(output_path), fourcc, video_fps, (out_size[0], out_size[1]))
+    for i in range(len(replay_frames[order[0]])):
+
+        frame_frames = []
+        for key in order:
+            if order == "real_fly":
+                frame_frames.append(replay_frames[key][i])
+            else:
+                frame_frames.appen(replay_frames[key][i][:, :, ::-1])
+        frame = cv2.hconcat(frame_frames)
+        out.write(frame)
+    
+    out.release()
+
+
+# run the main
+if __name__ == "__main__":
+    replay_frames = {}
+    replay_timestamps = {}
+    for xml_variant in ["deepfly3d_original", "deepfly3d"]:
+        data_block = get_data(xml_variant)
+        video_fps = 100
+
+        start_t = 3.5
+        end_t = 4.0#6
+        start_step = int(start_t / timestep)
+        end_step = int(end_t / timestep)
+        target_num_steps = end_step - start_step
+        data_block_focus = data_block[:, start_step:end_step]
+
+        replay_frames[xml_variant], replay_timestamps[xml_variant] = get_replay_frames(data_block_focus, video_fps)
+
+    replay_frames["real_fly"], replay_timestamps["real_fly"] = get_original_frames(start_t, end_t, video_fps)
+
+    write_side_by_side_video(["deepfly3d_original", "real_fly", "deepfly3d"],
+                                Path(f"/Users/stimpfli/Desktop/nmf2-paper/grooming/outputs/final_sidebyside.mp4"),
+                                replay_frames, replay_timestamps, video_fps)
+
---
+++ grooming/nmf_grooming.py
@@ -0,0 +1,224 @@
+from flygym import Fly, Camera
+from flygym.state import KinematicPose
+import flygym.preprogrammed as preprogrammed
+
+
+import pickle
+from pathlib import Path
+import numpy as np
+
+# List of all the joints that are actuated during grooming
+all_groom_dofs = (
+    [f"joint_{dof}" for dof in ["Head", "Head_yaw", "Head_roll"]]  # Head joints
+    + [
+        f"joint_{side}F{dof}"
+        for side in "LR"
+        for dof in [
+            "Coxa",
+            "Coxa_roll",
+            "Coxa_yaw",
+            "Femur",
+            "Femur_roll",
+            "Tibia",
+            "Tarsus1",
+        ]
+    ]  # Front leg joints
+    + [
+        f"joint_{side}{dof}{angle}"
+        for side in "LR"
+        for dof in ["Pedicel"]
+        for angle in ["", "_yaw"]
+    ]  # Antennae joints
+)
+
+# List of alL the bodies that might be colliding during groomming
+
+groom_self_collision = [
+    f"{side}{app}"
+    for side in "LR"
+    for app in [
+        "FTibia",
+        "FTarsus1",
+        "FTarsus2",
+        "FTarsus3",
+        "FTarsus4",
+        "FTarsus5",
+        "Arista",
+        "Funiculus",
+        "Pedicel",
+        "Eye",
+    ]
+]
+
+class GroomingFly(Fly):
+    def __init__(self, xml_variant, self_collisions=groom_self_collision, actuator_kp=10.0, contact_sensor_placements=preprogrammed.all_tarsi_links):
+        super().__init__(
+            xml_variant=xml_variant,
+            self_collisions=self_collisions,
+            actuator_kp=actuator_kp,
+            contact_sensor_placements=contact_sensor_placements,
+            init_pose=KinematicPose.from_yaml("./data/pose_groom.yaml"),
+            actuated_joints=all_groom_dofs,
+            floor_collisions = "none",
+        )
+
+    def _set_joints_stiffness_and_damping(self):
+        super()._set_joints_stiffness_and_damping()
+        for joint in self.model.find_all("joint"):
+            if any([app in joint.name for app in ["Pedicel", "Arista", "Funiculus"]]):
+                joint.stiffness = 1e-3
+                joint.damping = 1e-3
+
+        return None
+
+    def _set_actuators_gain(self):
+        for actuator in self.actuators:
+            if "Arista" in actuator.name:
+                kp = 1e-6
+            elif "Pedicel" in actuator.name or "Funiculus" in actuator.name:
+                kp = 0.2
+            else:
+                kp = self.actuator_kp
+            actuator.kp = kp
+        return None
+    
+    def _define_self_contacts(self, self_collisions_geoms):
+        # Only add relevant collisions:
+        # - No collisions between the two antennas
+        # - No collisions between segments in the same leg or antenna
+
+        self_contact_pairs = []
+        self_contact_pairs_names = []
+
+        for geom1_name in self_collisions_geoms:
+            for geom2_name in self_collisions_geoms:
+                body1 = self.model.find("geom", geom1_name).parent
+                body2 = self.model.find("geom", geom2_name).parent
+                simple_body1_name = body1.name.split("_")[0]
+                simple_body2_name = body2.name.split("_")[0]
+
+                body1_children = self.get_real_childrens(body1)
+                body2_children = self.get_real_childrens(body2)
+
+                body1_parent = self._get_real_parent(body1)
+                body2_parent = self._get_real_parent(body2)
+
+                geom1_is_antenna = any(
+                    [
+                        app in geom1_name
+                        for app in ["Pedicel", "Arista", "Funiculus", "Eye"]
+                    ]
+                )
+                geom2_is_antenna = any(
+                    [
+                        app in geom2_name
+                        for app in ["Pedicel", "Arista", "Funiculus", "Eye"]
+                    ]
+                )
+                is_same_side = geom1_name[0] == geom2_name[0]
+
+                if not (
+                    body1.name == body2.name
+                    or simple_body1_name in body2_children
+                    or simple_body2_name in body1_children
+                    or simple_body1_name == body2_parent
+                    or simple_body2_name == body1_parent
+                    or geom1_is_antenna
+                    and geom2_is_antenna  # both on antenna
+                    or is_same_side
+                    and (
+                        not geom1_is_antenna and not geom2_is_antenna
+                    )  # on the legs and same side
+                ):
+                    contact_pair = self.model.contact.add(
+                        "pair",
+                        name=f"{geom1_name}_{geom2_name}",
+                        geom1=geom1_name,
+                        geom2=geom2_name,
+                        solref=self.sim_params.contact_solref,
+                        solimp=self.sim_params.contact_solimp,
+                        margin=0.0,  # change margin to avoid penetration
+                    )
+                    self_contact_pairs.append(contact_pair)
+                    self_contact_pairs_names.append(f"{geom1_name}_{geom2_name}")
+
+        return self_contact_pairs, self_contact_pairs_names
+
+
+class GroomingCamera(Camera):
+    def __init__(self, fly, **kwargs):
+        super().__init__(fly, **kwargs)
+        self.update_camera_pos = False
+        self._zoom_camera()
+
+    
+    def _zoom_camera(self):
+        if self.camera_id == "0/camera_front":
+            self.cam_offset -= [
+                8-2.7,
+                0.0,
+                0.0,
+            ]
+        else: 
+            raise ValueError(f"Camera {self.camera_id} not recognized")
+
+    """def _add_force_sensors(self):
+        super()._add_force_sensors()
+        self._add_touch_sensors()
+
+    def _add_touch_sensors(self):
+        touch_sensors = []
+        for body_name in self.touch_sensor_locations:
+            body = self.model.find("body", body_name)
+            body_child_names = self.get_real_childrens(body)
+            if body_child_names:
+                body_child_name = body_child_names[0]
+                body_child = self.model.find("body", body_child_name)
+                next_body_pos = body_child.pos
+                if np.sum(np.abs(next_body_pos)) < 1e-3:
+                    next_body_pos = [0.0, 0.0, -0.2]
+            elif "Arista" in body_name:
+                if body_name[0] == "L":
+                    next_body_pos = [0.0, -0.2, 0.0]
+                else:
+                    next_body_pos = [0.0, 0.2, 0.0]
+            elif "Funiculus" in body_name:
+                next_body_pos = [0.0, 0.0, -0.2]
+            elif "Tarsus" in body_name:
+                next_body_pos = [0.0, 0.0, -0.2]
+            elif "Eye" in body_name:
+                pass
+            else:
+                raise ValueError(f"Body {body_name} has no children")
+            if "Eye" in body_name:
+                site = body.add(
+                    "site",
+                    type="sphere",
+                    name=f"{body_name}_touchsite",
+                    pos=next_body_pos,
+                    size=[1.0, 0.0, 0.0],
+                    rgba=[0.0, 0.0, 0.0, 0.0],
+                )
+            else:
+                quat = body.quat
+                site = body.add(
+                    "site",
+                    type="capsule",
+                    quat=quat,
+                    fromto=np.hstack((np.zeros(3), next_body_pos)),
+                    name=f"{body_name}_touchsite",
+                    pos=[0, 0, 0],
+                    size=[0.1, 0.0, 0.0],
+                    rgba=[0.0, 0.0, 0.0, 0.0],
+                )
+            touch_sensor = self.model.sensor.add(
+                "touch", name=f"touch_{body.name}", site=site.name
+            )
+            touch_sensors.append(touch_sensor)
+        self.touch_sensors = touch_sensors
+
+    def get_observation(self):
+        obs = super().get_observation()
+        touch_data = self.physics.bind(self.touch_sensors).sensordata
+        obs["touch_sensors"] = touch_data.copy()
+        return obs"""
