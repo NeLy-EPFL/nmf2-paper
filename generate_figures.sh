bca468076ffa8a2e0aec300f8fa62db47c89e6ad
Thomas Ka Chung Lam <tkclam>
2024-04-25 09:43:41 +0200

    update script

--- generate_figures.sh
+++ generate_figures.sh
@@ -1,112 +1,88 @@
 #!/bin/bash
-
 video_dir="_outputs/VIDEOS"
 figure_dir="_outputs/FIGURES"
 
-svid1="${video_dir}/Video1_ForcesLocomotion/video_1_force_visualization_v6_TL.mp4"
-svid2="${video_dir}/Video2_Climbing/video_2_climbing_v7_TL.mp4"
-svid3="${video_dir}/Video3_SingleStep/video_3_single_step_v5_TL.mp4"
-svid4="${video_dir}/Video4_CPG/video_4_cpg_controller_v9_TL.mp4"
-svid5="${video_dir}/Video5_RuleBased/video_5_rule_based_controller_v8_TL.mp4"
-svid6="${video_dir}/Video6_Hybrid/video_6_hybrid_controller_v8_TL.mp4"
-svid7="${video_dir}/Video7_ControllerCompare/video_7_controller_comparison_v9_TL_small.mp4"
-svid8="${video_dir}/Video8_VisualTaxis/video_8_visual_taxis_v14_TL.mp4"
-svid9="${video_dir}/Video9_OdorTaxis/video_9_odor_taxis_v8_TL.mp4"
-svid10="${video_dir}/Video10_MultimodalNavigationFollowCam/video_10_multimodal_navigation_example_v3_TL.mp4"
-svid11="${video_dir}/Video11_MultimodalNavigation9Trials/video_11_navigation_task_v6_TL.mp4"
+mkdir -p $video_dir
+mkdir -p $figure_dir
 
-edfig2="${figure_dir}/EDFig2_PreprogrammedStepping/edfig2_preprogrammed_stepping_v7_TL.pdf"
-edfig4="${figure_dir}/EDFig4_VisionModelRL/edfig4_vison_model_rl_v4_TL.pdf"
+vid2="${video_dir}/video_2_single_step_v5_TL.mp4"
+vid3="${video_dir}/video_3_force_visualization_v6_TL.mp4"
+vid4="${video_dir}/video_4_climbing_v7_TL.mp4"
+vid5="${video_dir}/video_5_cpg_controller_v9_TL.mp4"
+vid6="${video_dir}/video_6_rule_based_controller_v8_TL.mp4"
+vid7="${video_dir}/video_7_hybrid_controller_v8_TL.mp4"
+vid8="${video_dir}/video_8_controller_comparison_v10_TL_small.mp4"
+vid9="${video_dir}/video_9_visual_taxis_v14_TL.mp4"
+vid10="${video_dir}/video_10_odor_taxis_v8_TL.mp4"
+vid13="${video_dir}/video_13_multimodal_navigation_example_v3_TL.mp4"
 
-fig2_critical_slope="${figure_dir}/Fig2_AdhesionLocomotion/fig2_locomotion_critical_slope_v18_TL.pdf"
-fig2_controller_comparison="${figure_dir}/Fig2_AdhesionLocomotion/fig2_locomotion_controller_comparison_v19_TL.pdf"
+edfig2="${figure_dir}/edfig2_preprogrammed_stepping_v7_TL.pdf"
+edfig4="${figure_dir}/edfig4_vison_model_rl_v4_TL.pdf"
 
-fig3visual="${figure_dir}/Fig3_VisionOlfactionRL/fig3_sensory_visual_taxis_v14_TL.pdf"
-fig3visual_stable="${figure_dir}/Fig3_VisionOlfactionRL/fig3_sensory_visual_taxis_stable_v14_TL.pdf"
-fig3odor="${figure_dir}/Fig3_VisionOlfactionRL/fig3_sensory_odor_taxis_v14_TL.pdf"
-fig3trajectories="${figure_dir}/Fig3_VisionOlfactionRL/fig3_sensory_trajectories_v15_TL.pdf"
+fig2c="${figure_dir}/fig2_locomotion_critical_slope_v18_TL.pdf"
+fig2g="${figure_dir}/fig2_locomotion_controller_comparison_v19_TL.pdf"
 
-mkdir -p "$(dirname $svid1)"
-mkdir -p "$(dirname $svid2)"
-mkdir -p "$(dirname $svid3)"
-mkdir -p "$(dirname $svid4)"
-mkdir -p "$(dirname $svid5)"
-mkdir -p "$(dirname $svid6)"
-mkdir -p "$(dirname $svid7)"
-mkdir -p "$(dirname $svid8)"
-mkdir -p "$(dirname $svid9)"
-mkdir -p "$(dirname $svid10)"
-mkdir -p "$(dirname $svid11)"
-mkdir -p "$(dirname $edfig2)"
-mkdir -p "$(dirname $edfig4)"
-mkdir -p "$(dirname $fig2_controller_comparison)"
-mkdir -p "$(dirname $fig2_critical_slope)"
-mkdir -p "$(dirname $fig3visual)"
-mkdir -p "$(dirname $fig3visual_stable)"
-mkdir -p "$(dirname $fig3odor)"
+fig3c="${figure_dir}/fig3_sensory_visual_taxis_v14_TL.pdf"
+fig3d="${figure_dir}/fig3_sensory_odor_taxis_v14_TL.pdf"
 
-# supplementary video 1
-if [ ! -f $svid1 ]; then
-    cd leg_adhesion
-    jupyter nbconvert --to script force_visualization.ipynb
-    python force_visualization.py
-    rm force_visualization.py
-    mv outputs/force_visualization.mp4 "../$svid1"
-    cd ..
-fi
+fig5b="${figure_dir}/fig5_integration_trajectory_v14_TL.pdf"
+fig5c="${figure_dir}/fig5_integration_trajectories_v14_TL.pdf"
 
-# supplementary video 2
-if [ ! -f $svid2 ]; then
-    cd leg_adhesion
-    python generate_slope_datapts.py
-    python merge_videos.py
-    mv data/slope_front/climbing.mp4 "../$svid2"
-    cd ..
-fi
-
-# # supplementary video 3
-if [ ! -f $svid3 ] || [ ! -f $edfig2 ] ; then
+if [ ! -f $vid2 ] || [ ! -f $edfig2 ] ; then
     cd step_data
     jupyter nbconvert --to script stepping_illustration.ipynb
     python stepping_illustration.py
     rm stepping_illustration.py
-    mv outputs/single_step.mp4 "../$svid3"
+    mv outputs/single_step.mp4 "../$vid2"
     cp outputs/single_step.pdf "../$edfig2"
     cd ..
 fi
 
-# supplementary video 4
-if [ ! -f $svid4 ]; then
+if [ ! -f $vid3 ]; then
+    cd leg_adhesion
+    jupyter nbconvert --to script force_visualization.ipynb
+    python force_visualization.py
+    rm force_visualization.py
+    mv outputs/force_visualization.mp4 "../$vid3"
+    cd ..
+fi
+
+if [ ! -f $vid4 ]; then
+    cd leg_adhesion
+    python generate_slope_datapts.py
+    python merge_videos.py
+    mv data/slope_front/climbing.mp4 "../$vid4"
+    cd ..
+fi
+
+if [ ! -f $vid5 ]; then
     cd control_signal
     jupyter nbconvert --to script cpg.ipynb
     python cpg.py
     rm cpg.py
-    mv outputs/cpg.mp4 "../$svid4"
+    mv outputs/cpg.mp4 "../$vid5"
     cd ..
 fi
 
-# supplementary video 5
-if [ ! -f $svid5 ]; then
+if [ ! -f $vid6 ]; then
     cd control_signal
     jupyter nbconvert --to script rule_based.ipynb
     python rule_based.py
     rm rule_based.py
-    mv outputs/rule_based.mp4 "../$svid5"
+    mv outputs/rule_based.mp4 "../$vid6"
     cd ..
 fi
 
-# supplementary video 6
-if [ ! -f $svid6 ]; then
+if [ ! -f $vid7 ]; then
     cd control_signal
     jupyter nbconvert --to script hybrid.ipynb
     python hybrid.py
     rm hybrid.py
-    mv outputs/hybrid.mp4 "../$svid6"
+    mv outputs/hybrid.mp4 "../$vid7"
     cd ..
 fi
 
-# supplementary video 7 and figure 2G
-if [ ! -f $svid7 ] || [ ! -f $fig2_controller_comparison ]; then
+if [ ! -f $vid8 ] || [ ! -f $fig2g ]; then
     cd hybrid_controller
     python generate_datapts.py
     jupyter nbconvert --to script generate_figure.ipynb
@@ -115,45 +91,41 @@ if [ ! -f $svid7 ] || [ ! -f $fig2_controller_comparison ]; then
     python make_summary_video.py
     sh compress_video.sh
     rm outputs/controller_comparison.mp4
-    mv outputs/controller_comparison_small.mp4 "../$svid7"
-    cp outputs/speed_comparison.pdf "../$fig2_controller_comparison"
+    mv outputs/controller_comparison_small.mp4 "../$vid8"
+    cp outputs/speed_comparison.pdf "../$fig2g"
     cd ..
 fi
 
-# supplementary video 8 and figure 3C
-if [ ! -f $svid8 ] || [ ! -f $fig3visual ]; then
+if [ ! -f $vid9 ] || [ ! -f $fig3c ]; then
     cd visual_inputs
     jupyter nbconvert --to script visual_taxis.ipynb
     python visual_taxis.py 0
     rm visual_taxis.py
-    mv outputs/object_following_with_retina_images.mp4 "../$svid8"
-    cp outputs/visual_taxis.pdf "../$fig3visual"
+    mv outputs/object_following_with_retina_images.mp4 "../$vid9"
+    cp outputs/visual_taxis.pdf "../$fig3c"
     cd ..
 fi
 
-# supplementary video 9 and figure 3D
-if [ ! -f $svid9 ] || [ ! -f $fig3odor ]; then
+if [ ! -f $vid10 ] || [ ! -f $fig3d ]; then
     cd odor_inputs
     jupyter nbconvert --to script odor_taxis.ipynb
     python odor_taxis.py
     rm odor_taxis.py
-    mv outputs/odor_taxis.mp4 "../$svid9"
-    cp outputs/odor_taxis.pdf "../$fig3odor"
+    mv outputs/odor_taxis.mp4 "../$vid10"
+    cp outputs/odor_taxis.pdf "../$fig3d"
     cd ..
 fi
 
-# figure 2C
-if [ ! -f $fig2_critical_slope ]; then
+if [ ! -f $fig2c ]; then
     cd leg_adhesion
     python generate_gainslope_dataset_multiprocessing.py
     jupyter nbconvert --to script critical_angle_plot.ipynb
     python critical_angle_plot.py
     rm critical_angle_plot.py
-    cp outputs/critical_slope.pdf "../$fig2_critical_slope"
+    cp outputs/critical_slope.pdf "../$fig2c"
     cd ..
 fi
 
-# extended data figure 4
 if [ ! -f $edfig4 ]; then
     cd integrated_task
 
@@ -171,14 +143,18 @@ if [ ! -f $edfig4 ]; then
     cd ..
 fi
 
-# supplementary video 10 and 11
-if [ ! -f $svid10 ] || [ ! -f $svid11 ]; then
+if [ ! -f $vid13 ] || [ ! -f $fig5b ] || [ ! -f $fig5c ]; then
     cd integrated_task/preprint_trial
     python train_navigation_task.py
     python run_and_visualize.py
     python merge_videos.py
-    cp outputs/trajectories.pdf "../../$fig3trajectories"
-    mv outputs/example_out/video.mp4 "../../$svid10"
-    mv outputs/navigation_task_merged.mp4 "../../$svid11"
+
+    jupyter nbconvert --to script generate_figures.ipynb
+    python generate_figures.py
+    rm generate_figures.py
+
+    cp outputs/trajectory.pdf "../../$fig5b"
+    cp outputs/trajectories.pdf "../../$fig5c"
+    mv outputs/navigation_task_merged.mp4 "../../$vid13"
     cd ../..
 fi
